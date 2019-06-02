7dae702658df3662c7ba5245aba6577a5875cdea
michaelmitus <49987038+michaelmitus@users.noreply.github.com>
2019-06-02 15:11:51 +0500

    Add files via upload

--- api_server.py
+++ api_server.py
@@ -1,7 +1,8 @@
 import requests
 import sqlite3 as lite
-from flask import Flask, request, Response, render_template
-# jsonfy
+import json
+from newsapi import NewsApiClient
+from flask import Flask, request, Response, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -10,16 +11,22 @@ def add_user(id_vk, name):
     con = lite.connect('news_api.sqlite')
     curID = con.cursor()
     curID.executescript(sql_request)
-    print(id_vk, name)
-    print(curID.fetchall())
+    resp = curID.fetchone()
     con.close()
-    return Response('{"status": "ok"}', status=200, mimetype='application/json')
+    print(resp)
+    return str(resp)
+
+def get_user(vk_id):
+    sql_request = "SELECT * FROM Users WHERE id_vk = "+str(vk_id)
+    con = lite.connect('news_api.sqlite')
+    curID = con.cursor()
+    curID.execute(sql_request)
+    resp = curID.fetchone()
+    con.close()
+    return str(resp)
 
 @app.route('/users/<id>', methods=['GET', 'POST', 'PATCH', 'DELETE'])
-def users(*args, **kwars):
-    pickle_in = open('users_file.data', "rb")
-    data_in = pickle.load(pickle_in)
-    pickle_in.close()
+def users(*args, **kwargs):
     post_user = 'user'
     for user in data_in:
         if user['id'] == int(kwars['id']):
@@ -29,23 +36,13 @@ def users(*args, **kwars):
             post_parent = user['parent']
             post_id = user['id']
 
+@app.route('/users/', methods=['GET', 'POST', 'PATCH', 'DELETE'])
+def all_users(*args, **kwargs):
     if request.method == 'GET':
-        return '''
-            <html>
-              <head>
-                <title>User Info</title>
-              </head>
-              <body>
-                <h1> Name - , ''' + post_user + '''</h1>
-                <h1> age - , ''' + str(post_age) + '''</h1>
-                <h1> type - , ''' + post_type + '''</h1>
-                <h1> parent - , ''' + str(post_parent) + '''</h1>
-                <h1> id - , ''' + str(post_id) + '''</h1>
-              </body>
-            </html>
-            '''
+        return get_user(request.args.get('vk_id'))
     elif request.method == 'POST':
-        return add_user(**kwars)
+        print('Добавление в базу ',request.args.get('vk_id'),request.args.get('name'))
+        return add_user(request.args.get('vk_id'),request.args.get('name'))
     elif request.method == 'PATCH':
         return update_user(**kwars)
     elif request.method == 'DELETE':
@@ -53,6 +50,76 @@ def users(*args, **kwars):
     else:
         pass
 
+def get_all_category():
+#    def list_of_filters(params, **filters):
+    api_key = 'c6b4dff59361415dada7968f05685325'
+    newsapi = NewsApiClient(api_key)
+    all_sources = newsapi.get_sources()
+    sources = all_sources['sources']
+    selections = set()
+    for news_number in range(0, len(all_sources['sources'])):
+        selections.add(sources[news_number]['category'])
+    print(str(list(selections)))
+
+#    for news_number in range(0, len(selections)):
+#        print(str(news_number+1)+'. '+list(selections)[news_number])
+    print('0. Все')
+    return list(selections)
+
+def add_all_category():
+#    msg_text = "Все доступные категории новостей: \n"
+    selections = get_all_category()
+    for news_number in range(0, len(selections)):
+        try:
+            sql_request = "INSERT INTO Category (Text) VALUES ('%s')" % (selections[news_number])
+            con = lite.connect('news_api.sqlite')
+            curID = con.cursor()
+            curID.executescript(sql_request)
+            con.close()
+        except:
+            print('Ошибка добавления')
+            return "Error"
+    return 'Ok'
+
+def add_category(vk_id, category_id):
+#    Добавление подписок на категории
+    sql_request = "INSERT INTO Users_Category (CategoryID, UserID) VALUES ('%s','%s')" % (category_id, vk_id)
+    con = lite.connect('news_api.sqlite')
+    curID = con.cursor()
+    curID.executescript(sql_request)
+    con.close()
+    return 'Ok'
+
+def get_category(vk_id):
+    sql_request = '''
+                    SELECT Category.Text FROM Category
+                    JOIN Users_Category ON Category.ID = Users_Category.CategoryID 
+                    WHERE Users_Category.UserID = '''+str(vk_id)
+    con = lite.connect('news_api.sqlite')
+    curID = con.cursor()
+    curID.execute(sql_request)
+    resp = curID.fetchall()
+    con.close()
+    print(jsonify(resp))
+    return jsonify(resp)
+
+@app.route('/subscriptions/categories/', methods=['GET', 'POST', 'PATCH', 'DELETE'])
+def categorys(*args, **kwargs):
+    if request.method == 'GET':
+        return get_category(request.args.get('vk_id'))
+    elif request.method == 'POST':
+        if request.args.get('vk_id') == 0:
+            return add_all_category()
+        else:
+            return add_category(request.args.get('vk_id'),request.args.get('category_id'))
+    elif request.method == 'PATCH':
+        return update_user(**kwars)
+    elif request.method == 'DELETE':
+        return del_user(**kwars)
+    else:
+        return 'None'
+
+
+
 if __name__ == '__main__':
-   add_user(123424,'Иван Петров')
    app.run (host = '127.0.0.1', port = 8080)
--- vk_bot.py
+++ vk_bot.py
@@ -1,6 +1,8 @@
 import requests
 import vk_api
 import bs4
+import json
+from newsapi import NewsApiClient
 from random import randint
 from vk_api.longpoll import VkLongPoll, VkEventType
 
@@ -43,16 +45,15 @@ def get_user_name_from_vk_id(user_id):
     return user_name.split()[0]
 
 def add_user(user_id):
+    response = requests.post('http://localhost:8080/users/',
+                             params = {'vk_id': user_id, 'name': get_user_name_from_vk_id(user_id)})
+    print(response)
     pass
 
-def get_all_category(user_id):
-    pass
-
-def add_category(user_id):
-    pass
-
-def delete_category(user_id):
-    pass
+def get_user(user_id):
+    response = requests.get('http://localhost:8080/users/',
+                             params = {'vk_id': user_id})
+    return response.text
 
 def get_all_keywords(user_id):
     pass
@@ -66,43 +67,175 @@ def delete_keywords(user_id):
 def get_news(user_id):
     pass
 
+def vk_print(user_id, title, menu_items):
+    msg_text = title + '\n'
+    for items in range(len(menu_items)):
+        msg_text = msg_text + (str(items + 1) + '. ' + menu_items[items] + '\n')
+    write_msg(user_id=user_id,
+              message=msg_text)
+
+def vk_menu(user_id, title, menu_items):
+    vk_print(user_id, title, menu_items)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            # Слушаем longpoll, если пришло сообщение то:
+            if event.from_user or event.from_chat:  # Если написали
+                return event.text
+
+def registration(user_id):
+    while True:
+        result_choice = vk_menu(user_id,
+                                'Регистрация:',
+                                ('Добавить пользователя', 'Изменить пользователя', 'Удалить пользователя', 'Выход'))
+        if result_choice in ('1', 'Добавить пользователя'):
+            write_msg(user_id=user_id,
+                      message='Вы уже зарегистрированы')
+            return 1
+        elif result_choice in ('2', 'Изменить пользователя'):
+            write_msg(user_id=user_id,
+                      message='Не хватает прав доступа')
+            return 2
+        elif result_choice in ('3', 'Удалить пользователя'):
+            write_msg(user_id=user_id,
+                      message='Не хватает прав доступа')
+            return 3
+        elif result_choice in ('0', '4'):
+            return 0
+
+def get_all_category():
+#    def list_of_filters(params, **filters):
+    api_key = 'c6b4dff59361415dada7968f05685325'
+    newsapi = NewsApiClient(api_key)
+    all_sources = newsapi.get_sources()
+    sources = all_sources['sources']
+    selections = set()
+    for news_number in range(0, len(all_sources['sources'])):
+        selections.add(sources[news_number]['category'])
+    return list(selections)
+
+def get_category(user_id):
+    response = requests.get('http://localhost:8080/subscriptions/categories/',
+                             params = {'vk_id': user_id})
+    todos = json.loads(response.text)
+    print(response)
+    print(todos)
+    print(type(todos))
+    print(type(response.text))
+    return todos
+    pass
+
+def add_category(user_id,category_id):
+    response = requests.post('http://localhost:8080/subscriptions/categories/',
+                             params = {'vk_id': user_id, 'category_id': category_id})
+    return response.text
+    pass
+
+def delete_category(user_id):
+    pass
+
+def category_of_news(user_id):
+    while True:
+        result_choice = vk_menu(user_id,
+                                'Работа с категориями:',(
+                                    'Посмотреть все доступные категории',
+                                    'Посмотреть подписки на категории',
+                                    'Добавить подписку на категорию',
+                                    'Удалить подписку на категорию',
+                                    'Выход'
+                                 ))
+        if result_choice in ('1', 'Посмотреть все доступные категории'):
+            msg_text = 'Все доступные категории' + '\n'
+            selection = list(get_all_category())
+            for items in range(len(selection)):
+                msg_text = msg_text + selection[items] + '\n'
+            write_msg(user_id=user_id,
+                      message=msg_text)
+
+        if result_choice in ('2', 'Посмотреть подписки на категории'):
+            write_msg(user_id=user_id,
+                      message=get_category(user_id))
+            selection = list(get_category(user_id))
+            msg_text = ' '
+            for items in range(len(selection)):
+                msg_text = msg_text + str(selection[items])+ '\n'
+            write_msg(user_id=user_id,
+                      message=msg_text)
+
+
+
+
+        elif result_choice in ('20', 'Добавить все категории'):
+            add_category(0, 0)
+            return 20
+        elif result_choice in ('0', 'Выход'):
+            return 0
+
+#elif event.text == 'Посмотреть подписки категории' or event.text == '2.1':
+#get_category(event.user_id)
+#elif event.text == 'Добавить подписку на категорию' or event.text == '2.2':
+#add_category(event.user_id)
+#elif event.text == 'Удалить подписку на категорию' or event.text == '2.3':
+#delete_category(event.user_id)
+
+
+def main_menu(user_id):
+    while True:
+        result_choice = vk_menu(event.user_id,
+                                'Список разделов:',
+                                ('Регистрация', 'Категории новостей', 'Ключевые слова', 'Новости'))
+        if result_choice in ('1', 'Регистрация'):
+            registration(event.user_id)
+            return 1
+
+        elif result_choice in ('2', 'Категории новостей'):
+            category_of_news(user_id)
+            return 2
+
+
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
    #Слушаем longpoll, если пришло сообщение то:
         if event.from_user or event.from_chat: #Если написали
-            if event.text == 'Привет' or event.text == 'Hi':  # Если написали заданную фразу
-                vk_username = get_user_name_from_vk_id(event.user_id)
-                write_msg(user_id=event.user_id,
-                          message='Привет, '+vk_username,
-                          )
-            elif event.text == 'Список команд':
-                write_msg(user_id=event.user_id,
-                          message='''
-                            Список команд:
-                            1. Добавить пользователя
-                            2. Категории новостей
-                            2.1. Посмотреть категории
-                            2.2. Добавить категорию
-                            2.3. Удалить категорию
-                            3. Ключевые слова 
-                            3.1. Посмотреть ключевые слова
-                            3.2. Добавить ключевые слова
-                            3.3. Удалить ключевые слова
-                            4. Получить новости                                 
-                         ''')
-            elif event.text == 'Добавить пользователя' or event.text == '1':
-                add_user(event.user_id)
-            elif event.text == 'Посмотреть категории' or event.text == '2.1':
-                get_all_category(event.user_id)
-            elif event.text == 'Добавить категорию' or event.text == '2.2':
-                add_category(event.user_id)
-            elif event.text == 'Удалить категорию' or event.text == '2.3':
-                delete_category(event.user_id)
-            elif event.text == 'Посмотреть ключевые слова' or event.text == '3.1':
-                get_all_keywords(event.user_id)
-            elif event.text == 'Добавить ключевые слова' or event.text == '3.2':
-                add_keywords(event.user_id)
-            elif event.text == 'Удалить ключевые слова' or event.text == '3.3':
-                delete_keywords(event.user_id)
-            elif event.text == 'Получить новости' or event.text == '4':
-                get_news(event.user_id)
+            if get_user(event.user_id)=='None':
+                if event.text == 'Добавить пользователя' or event.text == '1':
+                    add_user(event.user_id)
+                    vk_username = get_user_name_from_vk_id(event.user_id)
+                    write_msg(user_id=event.user_id,
+                              message='Отлично, ' + vk_username + ', теперь ты в наших рядах',
+                              )
+                else:
+                    vk_username = get_user_name_from_vk_id(event.user_id)
+                    write_msg(user_id=event.user_id,
+                              message='Привет, '+vk_username)
+                    result_choice = vk_menu(event.user_id,
+                                            'Тебя пока нет в нашей базе данных, желаешь зарегистрироваться?',
+                                            ('Зарегистрироваться','Нет'))
+            else:
+                if event.text == 'Привет' or event.text == 'Hi':  # Если написали заданную фразу
+                    vk_username = get_user_name_from_vk_id(event.user_id)
+                    write_msg(user_id=event.user_id,
+                              message='Привет, '+vk_username,
+                              )
+                elif event.text == 'Список команд' or event.text == '0':
+                    main_menu(event.user_id)
+
+
+                elif event.text == 'Проверка':
+                    write_msg(user_id=event.user_id,
+                              message=get_user(event.user_id))
+
+#                    msg_text = "Все доступные категории новостей: \n"
+#                    for news_number in range(0, len(selections)):
+#                        msg_text = msg_text+(str(news_number + 1) + '. ' + selections[news_number]+'\n')
+#                    msg_text = msg_text + ('0. Все' )
+#                    write_msg(user_id=event.user_id,
+#                              message=msg_text
+#                              )
+                elif event.text == 'Посмотреть ключевые слова' or event.text == '3.1':
+                    get_all_keywords(event.user_id)
+                elif event.text == 'Добавить ключевые слова' or event.text == '3.2':
+                    add_keywords(event.user_id)
+                elif event.text == 'Удалить ключевые слова' or event.text == '3.3':
+                    delete_keywords(event.user_id)
+                elif event.text == 'Получить новости' or event.text == '4':
+                    get_news(event.user_id)
